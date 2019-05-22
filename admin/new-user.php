55b483bf080d027f519ebc6e688b5dd5eee06c18
vithayben <vithayben@gmail.com>
2019-05-22 14:45:52 +0700

    working with create user and clear code register

--- admin/new-user.php
+++ admin/new-user.php
@@ -8,7 +8,48 @@
   $msg = '';
   $check_required = false;
   if(isset($_POST['add_user'])) {
-
+    if($_POST['firstname'] == '' || $_POST['lastname'] == '' || $_POST['email'] == '' || $_POST['password'] == '' || $_POST['con_password'] == '' || $_POST['address'] == '') {
+      $check_required = true;
+    } else {
+      $firstname = trim($_POST['firstname']);
+      $lastname = trim($_POST['lastname']);
+      $email = trim($_POST['email']);
+      $fullname = $firstname . $lastname;
+      if(trim($_POST['password']) === trim($_POST['con_password'])) {
+        $password = password_hash(trim($_POST['password']), PASSWORD_DEFAULT);
+        $gender = $_POST['gender'];
+        $address = $_POST['address'];
+        $phone = $_POST['phone'];
+        $city = $_POST['city'];
+        $country = $_POST['country'];
+        $dob = $_POST['dob'];
+        $status = $_POST['status'];
+        $role = $_POST['role'];
+        $about = $_POST['about'];
+        $quote = $_POST['quote'];
+        $created_date = date("Y-m-d h:i:s");
+        $updated_date = date("Y-m-d h:i:s");
+        $sql = "SELECT * FROM users WHERE LOWER(fullname) = LOWER('$fullname')";
+        $result = mysqli_query($conn, $sql);
+        if(mysqli_num_rows($result) != 0) {
+          $error = 'User already exist!';
+        } else {
+          if($role != '' && $status != '') {
+            $stmt = "INSERT INTO users(firstname, lastname, fullname, email, gender, password, phone, dob, address, city, country, about, quote, role_id, status, created_date, updated_date) 
+            VALUES('$firstname', '$lastname', '$fullname', '$email', '$gender', '$password', '$phone', '$dob', '$address', '$city', '$country', '$about', '$quote', $role, '$status', '$created_date', '$updated_date')";
+            if($conn->query($stmt) === true) {
+              $msg = 'New record created successfully';
+            } else {
+              $error = "Error: " . $conn->error;
+            }
+          } else {
+            $error = "No default role and status!";
+          }
+        }
+      } else {
+        $error = 'Password and Confirmation Password are not the same please check again!';
+      }
+    }
   }
 ?>
 
@@ -18,6 +59,8 @@
           <div class="card card-info">
             <div class="card-header">
               <h4 class="header">Add User</h4>
+              <h4 class="text-danger"><?php echo $error != '' ? $error : ''; ?></h4>
+              <h4 class="text-success"><?php echo $msg != '' ? $msg : ''; ?></h4>
             </div>
             <div class="card-body">
               <form action="new-user.php" method="post">
@@ -118,14 +161,20 @@
                       <input type="text" name="phone" class="form-control">
                     </div>
                   </div>
-                  <div class="col-sm-12">
+                  <div class="col-sm-6">
                     <div class="form-group">
                       <label class="info-name">About Me</label>
                       <textarea name="about" class="form-control" rows="3"></textarea>
-                        <div class="text-right btn-wrap">
-                          <a href="" class="btn btn-danger btn-mar">Cancel</a>
-                          <input type="submit" name="add_user" class="btn btn-primary btn-mar" value="Add">
-                        </div>
+                    </div>
+                  </div>
+                  <div class="col-sm-6">
+                    <div class="form-group">
+                      <label class="info-name">Quote</label>
+                      <textarea name="quote" class="form-control" rows="3"></textarea>
+                      <div class="text-right btn-wrap">
+                        <a href="" class="btn btn-danger btn-mar">Cancel</a>
+                        <input type="submit" name="add_user" class="btn btn-primary btn-mar" value="Add">
+                      </div>
                     </div>
                   </div>
                 </div>
